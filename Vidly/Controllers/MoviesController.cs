fd7dfed36c02df99c059d2cb63035525c3343a74
Adam <adamski01231@gmail.com>
2017-12-17 22:15:15 +0100

    Added some Movie properties and details page.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,30 +11,33 @@ namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         public ActionResult Index()
         {
-            var movies = GetMoveies();
+            var movies = _context.Movies.Include(m => m.Genre);
 
             return View(movies);
         }
 
         public ActionResult Details(int id)
         {
-            var movie = GetMoveies().SingleOrDefault(m => m.Id == id);
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
 
             if (movie == null)
                 return HttpNotFound();
 
             return View(movie);
-        }
-
-        private IEnumerable<Movie> GetMoveies()
-        {
-            return new List<Movie>
-            {
-                new Movie{ Id = 1, Name = "Shrek" },
-                new Movie{ Id = 2, Name = "Wall-e" },
-            };
         }
     }
 }
