6e6571cb29625a65a572d61a414990cf733641dd
Adam <adamski01231@gmail.com>
2017-12-17 16:24:50 +0100

    Display the list of customers and movies.

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public ActionResult Index()
+        {
+            var movies = GetMoveies();
+
+            return View(movies);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMoveies().SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMoveies()
+        {
+            return new List<Movie>
+            {
+                new Movie{ Id = 1, Name = "Shrek" },
+                new Movie{ Id = 2, Name = "Wall-e" },
+            };
+        }
+    }
+}
